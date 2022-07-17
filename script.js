63dfa493ad016e65f72a9da0e7feced4d35bec9a
Marcin <marcinstarzomski@gmail.com>
2022-07-17 21:01:05 +0200

    Add operate function

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+// Operate function. Takes an operator and two numbers then takes one of the above functions. Executed when user click equal sign.
+
+function operate(num1, num2, operator) {
+    if (operator == true) {
+        return add(num1, num2);
+    } else if (operator == true) {
+        return subtract(num1, num2);
+    } else if (operator == true) {
+        return multiply(num1, num2)
+    } else if (operator == true) {
+        return divide(num1, num2)
+    } else console.log('ERR')
+}
