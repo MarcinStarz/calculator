25ff7f7598ca7f71f205f0e2a3b77678c7df3100
Marcin <marcinstarzomski@gmail.com>
2022-07-21 12:19:27 +0200

    Add input operator

--- script.js
+++ script.js
@@ -16,16 +16,45 @@ function divide(a, b) {
     return a/b;
 }
 
-// Operate function. Takes an operator and two numbers then takes one of the above functions. Executed when user click equal sign.
+// Operate function. Takes an operator and two numbers then takes one of the above functions. Executed when user clicks the equal sign.
 
 function operate(num1, num2, operator) {
-    if (operator == true) {
+    if (operator == 'add') {
         return add(num1, num2);
-    } else if (operator == true) {
+    } else if (operator == 'subtract') {
         return subtract(num1, num2);
-    } else if (operator == true) {
+    } else if (operator == 'multiply') {
         return multiply(num1, num2)
-    } else if (operator == true) {
+    } else if (operator == 'divide') {
         return divide(num1, num2)
     } else console.log('ERR')
 }
+
+// Number input listener function.
+
+function inputNumber() {
+    let value = [];
+    const buttonDigit = document.querySelectorAll('.button__digit');
+    for (let i = 0; i < buttonDigit.length; i++) {
+        buttonDigit[i].addEventListener('click', function(e) {
+            value.push(e.target.innerHTML);
+            console.log(value);
+        });
+    }
+}
+
+// Operator input listener function.
+
+function inputOperator() {
+    let operator = 0;
+    const buttonOperator = document.querySelectorAll('.button__operator');
+    for (let i = 0; i < buttonOperator.length; i++) {
+        buttonOperator[i].addEventListener('click', function(e) {
+            operator = e.target.innerHTML;
+            console.log(operator);
+        });
+    }
+}
+
+inputNumber();
+inputOperator();
